d3fce2183a34206c394eb6c71c677c2c20abb374
nikamanthab <nitinnikamanth@gmail.com>
2024-01-03 20:28:10 -0500

    49

--- 49 Group Anagrams.py
+++ 49 Group Anagrams.py
@@ -1,5 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # time - O(m*n), space - O(n)
         d = {}
         for s in strs:
             key = [0]*26
